fef0c7c9da84b1b506ddf8fd256068c72b03e1ed
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-26 18:36:51 +0100

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./css/main.css";
 
+import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+
 function App() {
-  return <div className="App">Laundry</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/SignIn/SignIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SignIn = () => {
+  return <div className="signin">Sign In</div>;
+};
+
+export default SignIn;
