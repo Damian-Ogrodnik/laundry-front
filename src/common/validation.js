8589cf164d2c557fcc771ece7602e4a9bf8d395a
dogrodnik <d.ogrodnik96@gmail.com>
2020-02-02 10:25:15 +0100

    Add sing up form

---
+++ src/common/validation.js
@@ -0,0 +1,21 @@
+import * as Yup from "yup";
+
+export const registrationSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+  lastName: Yup.string()
+    .max(20, "Must be 20 characters or less")
+    .required("Required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Required"),
+  password: Yup.string().required("This field is required"),
+  repassword: Yup.string().when("password", {
+    is: val => (val && val.length > 0 ? true : false),
+    then: Yup.string().oneOf(
+      [Yup.ref("password")],
+      "Both password need to be the same"
+    )
+  })
+});
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,5 +1,44 @@
 import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+
+import { registrationSchema } from "../../common/validation";
 
 export const SignUp = () => {
-  return <div className="signup">Sign Up</div>;
+  return (
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        repassword: ""
+      }}
+      validationSchema={registrationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" type="text" />
+        <ErrorMessage name="firstName" />
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" type="text" />
+        <ErrorMessage name="lastName" />
+        <label htmlFor="email">Email Address</label>
+        <Field name="email" type="email" />
+        <ErrorMessage name="email" />
+        <label htmlFor="password">Password</label>
+        <Field name="password" type="password" />
+        <ErrorMessage name="password" />
+        <label htmlFor="repassword">Confirm Password</label>
+        <Field name="repassword" type="password" />
+        <ErrorMessage name="repassword" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
 };
