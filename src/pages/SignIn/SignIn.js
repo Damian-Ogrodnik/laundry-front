5485e425ce7187c0a3d818eeb209baa9818ee7bd
dogrodnik <d.ogrodnik96@gmail.com>
2020-02-05 18:49:49 +0100

    Create signin page

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,7 +1,9 @@
 import React from "react";
-
-import { login } from "../../data/login";
 import { Redirect } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+
+import { registrationSchema } from "../../common/validation";
+import { login } from "../../data/login";
 
 export const SignIn = () => {
   const handleClick = () => {
@@ -11,9 +13,39 @@ export const SignIn = () => {
 
   const renderButton = () => {
     return (
-      <div className="signin">
-        <button onClick={() => handleClick()}>Login</button>
-      </div>
+      <Formik
+        initialValues={{
+          nickName: "",
+          password: ""
+        }}
+        validationSchema={registrationSchema}
+        onSubmit={() => {
+          console.log(1);
+          handleClick();
+        }}
+      >
+        <Form className="signup">
+          <div className="form__wrapper">
+            <div className="form__input">
+              <label htmlFor="nickName">Nickname</label>
+              <Field name="nickName" type="text" />
+              <ErrorMessage name="nickName">
+                {msg => <div className="form__error">{msg}</div>}
+              </ErrorMessage>
+            </div>
+            <div className="form__input">
+              <label htmlFor="password">Password</label>
+              <Field name="password" type="password" />
+              <ErrorMessage name="password">
+                {msg => <div className="form__error">{msg}</div>}
+              </ErrorMessage>
+            </div>
+            <button className="form__button" type="submit">
+              Login
+            </button>
+          </div>
+        </Form>
+      </Formik>
     );
   };
 
