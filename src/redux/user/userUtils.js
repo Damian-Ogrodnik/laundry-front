c0388480f11016bf00e8a20331a6e20ac75a4cbd
dogrodnik <d.ogrodnik96@gmail.com>
2020-02-13 12:55:16 +0100

    Handle errors message from the server

--- src/redux/user/userUtils.js
+++ src/redux/user/userUtils.js
@@ -5,7 +5,12 @@ import { signIn, signUp } from "../../services/User";
 export const logUser = (name, password) => async dispatch => {
   try {
     dispatch(actions.login());
-    const response = await signIn(name, password);
+    const response = await signIn(name, password).catch(err => {
+      if (err.response) {
+        throw new Error(err.response.data.errors[0].msg);
+      }
+      throw new Error("Internal Server Error");
+    });
     await dispatch(actions.loginSuccess(response));
   } catch (error) {
     dispatch(actions.loginFailure(error.message));
@@ -19,7 +24,13 @@ export const registerUser = ({
 }) => async dispatch => {
   try {
     dispatch(actions.login());
-    const response = await signUp(nickName, password, email);
+    const response = await signUp(nickName, password, email).catch(err => {
+      console.log(err.response.status);
+      if (err.response.status === 500) {
+        throw new Error("Name or email has been taken");
+      }
+      throw new Error("Internal Server Error");
+    });
     await dispatch(actions.loginSuccess(response));
   } catch (error) {
     dispatch(actions.loginFailure(error.message));
