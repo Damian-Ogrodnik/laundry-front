7367be6581372117de853c11b812aed8ea3d26f8
dogrodnik <d.ogrodnik96@gmail.com>
2020-02-20 09:19:00 +0100

    Create slots component

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -1,25 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
-import { Slot } from "../Slot";
-
-import { slots } from "../../utils/slots";
+import { Slots } from "../Slots";
 
 export const Booking = () => {
-  const takenSlots = useSelector(state => state.board.bookings);
-  const selectedSlot = useSelector(state => state.board.selectedSlot);
-
-  const renderSlots = () => {
-    return slots.map(({ hours, number }) => {
-      let taken, selected;
-      if (takenSlots)
-        taken = takenSlots.some(takenSlot => number === takenSlot.number);
-      if (selectedSlot === number) selected = true;
-      return (
-        <Slot hours={hours} number={number} taken={taken} selected={selected} />
-      );
-    });
-  };
-
-  return <div className="booking">{renderSlots()}</div>;
+  return (
+    <div className="booking">
+      <Slots />
+      <button className="booking__button">Book</button>
+    </div>
+  );
 };
---
+++ src/components/Slots/Slots.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { Slot } from "../Slot";
+
+import { slots } from "../../utils/slots";
+
+export const Slots = () => {
+  const takenSlots = useSelector(state => state.board.bookings);
+  const selectedSlot = useSelector(state => state.board.selectedSlot);
+
+  const renderSlots = () => {
+    return slots.map(({ hours, number }) => {
+      let taken, selected;
+      if (takenSlots)
+        taken = takenSlots.some(takenSlot => number === takenSlot.number);
+      if (selectedSlot === number) selected = true;
+      return (
+        <Slot hours={hours} number={number} taken={taken} selected={selected} />
+      );
+    });
+  };
+  return <div className="booking__slots">{renderSlots()}</div>;
+};
