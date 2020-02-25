33fa3c0c789e0d4f3a312ef7590066359ab36a93
dogrodnik <d.ogrodnik96@gmail.com>
2020-02-25 07:45:27 +0100

    Remove old dates from the user bookings board

--- src/services/Date.js
+++ src/services/Date.js
@@ -1,10 +1,22 @@
 export const sortByDates = async bookings => {
-  return await bookings.sort((aBooking, bBooking) => {
+  let sortedBookings = await bookings.sort((aBooking, bBooking) => {
     let aArray = aBooking.date.split("-");
     let bArray = bBooking.date.split("-");
     return (
       new Date(bArray[2], bArray[1] - 1, bArray[0]) -
       new Date(aArray[2], aArray[1] - 1, aArray[0])
+    );
+  });
+
+  return await removeOldDates(sortedBookings);
+};
+
+const removeOldDates = async bookings => {
+  return await bookings.filter(booking => {
+    let bookingArray = booking.date.split("-");
+    return (
+      new Date(bookingArray[2], bookingArray[1] - 1, bookingArray[0]) >
+      new Date()
     );
   });
 };
