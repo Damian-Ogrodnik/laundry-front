cc5cba2341e863d2cade7478581dfaa28401f350
dogrodnik <d.ogrodnik96@gmail.com>
2020-02-27 10:49:19 +0100

    Style homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
     <div className="homepage">
-      <h1>Home Page</h1>
+      <div className="homepage__front">
+        <div className="homepage__welcome">
+          <h2>Welcome at</h2>
+          <h1 className="homepage__header">Student Dorm Laundry</h1>
+        </div>
+        <div className="homepage__buttons">
+          <Link to="/sign-in">
+            <button className="button__signin">Sign in</button>
+          </Link>
+          <Link to="/sign-up">
+            <button className="button__signup">Sign up</button>
+          </Link>
+        </div>
+      </div>
+      <div className="homepage__instruction"></div>
+      <div className="homepage__footer"></div>
     </div>
   );
 };
