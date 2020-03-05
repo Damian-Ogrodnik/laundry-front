86184fcd746aac5f222c91d5a35c4596279821eb
dogrodnik <d.ogrodnik96@gmail.com>
2020-03-05 08:13:42 +0100

    Create Front component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,28 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import { Footer } from "../../components/Footer";
+import { Front } from "../../components/Front";
 import { Instruction } from "../../components/Instruction";
 
 export const Home = () => {
   return (
     <div className="homepage">
-      <div className="homepage__front">
-        <div className="homepage__welcome">
-          <h2>Welcome at</h2>
-          <h1 className="homepage__header">
-            Student <span>Dorm</span> Laundry
-          </h1>
-        </div>
-        <div className="homepage__buttons">
-          <Link to="/sign-in">
-            <button className="button__signin">SIGN IN</button>
-          </Link>
-          <Link to="/sign-up">
-            <button className="button__signup">SIGN UP</button>
-          </Link>
-        </div>
-      </div>
+      <Front />
       <Instruction />
       <Footer />
     </div>
