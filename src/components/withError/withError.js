99bba3be1b2c3bf79db04deedd6fec3226a06111
dogrodnik <d.ogrodnik96@gmail.com>
2020-03-18 14:45:01 +0100

    HOC loading change

--- src/components/withError/withError.js
+++ src/components/withError/withError.js
@@ -1,15 +1,11 @@
 import React from "react";
 
 export const withError = WrappedComponent => ({ error, loading, ...props }) => {
-  const renderComponent = () => {
-    if (loading) {
-      return <div>Loading...</div>;
-    } else if (error) {
-      return <div>Ooops... Something went wrong. Try again later.</div>;
-    } else {
-      return <WrappedComponent {...props} />;
-    }
-  };
-
-  return renderComponent();
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>Ooops... Something went wrong. Try again later.</div>
+  ) : (
+    <WrappedComponent {...props} />
+  );
 };
