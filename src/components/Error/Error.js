23ad5577046bfc9080b0b03e79d0486ae5da4f0f
dogrodnik <d.ogrodnik96@gmail.com>
2020-03-19 11:42:57 +0100

    Modify error component

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { IconContext } from "react-icons";
+import { MdError } from "react-icons/md";
 
-export const Error = ({ errorMsg, name }) => {
-  return <div className={`${name} error`}>{errorMsg}</div>;
+export const Error = ({ errorMsg, name, icon }) => {
+  return (
+    <div className={`${name} error`}>
+      {icon && (
+        <IconContext.Provider value={{ className: `${name} error__icon` }}>
+          <MdError />
+        </IconContext.Provider>
+      )}
+      <p>{errorMsg}</p>
+    </div>
+  );
 };
--- src/components/withError/withError.js
+++ src/components/withError/withError.js
@@ -1,11 +1,17 @@
 import React from "react";
+
 import { Loader } from "../Loader";
+import { Error } from "../Error";
 
 export const withError = WrappedComponent => ({ error, loading, ...props }) => {
   return loading ? (
     <Loader />
   ) : error ? (
-    <div>Ooops... Something went wrong. Try again later.</div>
+    <Error
+      errorMsg={"Ooops... Something went wrong. Try again later."}
+      name={"main"}
+      icon={true}
+    />
   ) : (
     <WrappedComponent {...props} />
   );
