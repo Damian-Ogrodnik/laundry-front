d8e1b548afab095d6063d3015212c76da353ab09
dogrodnik <d.ogrodnik96@gmail.com>
2020-03-20 09:49:58 +0100

    Add change password form to the user page

--- src/common/validation.js
+++ src/common/validation.js
@@ -23,3 +23,17 @@ export const loginSchema = Yup.object().shape({
   nickName: Yup.string().required("Required"),
   password: Yup.string().required("Required")
 });
+
+export const changePasswordSchema = Yup.object().shape({
+  password: Yup.string().required("Required"),
+  repassword: Yup.string()
+    .required("Required")
+    .min(8, "Password must be at least 8 characters"),
+  repasswordConfirm: Yup.string().when("repassword", {
+    is: val => (val && val.length > 0 ? true : false),
+    then: Yup.string().oneOf(
+      [Yup.ref("repassword")],
+      "Both password need to be the same"
+    )
+  })
+});
--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,11 +1,54 @@
 import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+
+import { changePasswordSchema } from "../../common/validation";
 
 import { Logout } from "../../components/Logout";
 
 export const User = () => {
   return (
     <div className="user-page">
-      <h1>User Board</h1>
+      <Formik
+        initialValues={{
+          password: "",
+          repassword: "",
+          repasswordConfirm: ""
+        }}
+        validationSchema={changePasswordSchema}
+        onSubmit={() => {
+          console.log("Password changed");
+        }}
+      >
+        <Form className="signup">
+          <div className="form__wrapper">
+            <h2>Change Password</h2>
+            <div className="form__input">
+              <label htmlFor="password">Password</label>
+              <Field name="password" type="password" />
+              <ErrorMessage name="password">
+                {msg => <div className="form__error">{msg}</div>}
+              </ErrorMessage>
+            </div>
+            <div className="form__input">
+              <label htmlFor="repassword">New Paswordd</label>
+              <Field name="repassword" type="password" />
+              <ErrorMessage name="repassword">
+                {msg => <div className="form__error">{msg}</div>}
+              </ErrorMessage>
+            </div>
+            <div className="form__input">
+              <label htmlFor="repasswordConfirm">Confirm New Password</label>
+              <Field name="repasswordConfirm" type="password" />
+              <ErrorMessage name="repasswordConfirm">
+                {msg => <div className="form__error">{msg}</div>}
+              </ErrorMessage>
+            </div>
+            <button className="form__button" type="submit">
+              CHANGE
+            </button>
+          </div>
+        </Form>
+      </Formik>
       <Logout name={"logout"} />
     </div>
   );
