4307261475975c9df7dd7b0b6158249d8dfd2420
dogrodnik <d.ogrodnik96@gmail.com>
2020-03-22 09:59:23 +0100

    Modal style in CSS

--- src/components/withConfirm/withConfirm.js
+++ src/components/withConfirm/withConfirm.js
@@ -1,20 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 
-const style = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    borderRadious: "5%",
-    border: "0.2rem solid black",
-    backgroundColor: "rgb(201, 193, 193)"
-  }
-};
-
 export const withConfirm = WrappedComponent => ({ ...props }) => {
   const [open, setOpen] = useState(false);
   const [confirm, setConfirm] = useState(false);
@@ -23,7 +9,12 @@ export const withConfirm = WrappedComponent => ({ ...props }) => {
 
   return (
     <>
-      <Modal isOpen={open} contentLabel="Confirmation" style={style}>
+      <Modal
+        isOpen={open}
+        contentLabel="Confirmation"
+        className="modal"
+        overlayClassName="overlay"
+      >
         <h2>Are you sure?</h2>
         <button
           onClick={() => {
