1a3e0542f4ba909d154c3c7edef007ace2e9d938
dogrodnik <d.ogrodnik96@gmail.com>
2020-03-25 08:55:04 +0100

    Refactor NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,41 +2,30 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
+const loggedNav = [
+  ["Account", "/user"],
+  ["Book", "/booking"],
+  ["My Bookings", "/user-bookings"]
+];
+const unloggedNav = [
+  ["Home", "/home"],
+  ["Sign In", "/sign-in"],
+  ["Sign Up", "/sign-up"]
+];
+
 export const NavBar = () => {
   const logged = useSelector(state => state.user.logged);
-
-  const renderNavBar = () => {
-    return logged ? (
-      <ul className="navigation__links">
-        <NavLink activeClassName="selected" to="/user">
-          <li>Account</li>
-        </NavLink>
-        <NavLink activeClassName="selected" to="/booking">
-          <li>Book</li>
-        </NavLink>
-        <NavLink activeClassName="selected" to="/user-bookings">
-          <li>My Bookings</li>
-        </NavLink>
-      </ul>
-    ) : (
-      <ul className="navigation__links">
-        <NavLink activeClassName="selected" to="/home">
-          <li>Home</li>
-        </NavLink>
-        <NavLink activeClassName="selected" to="/sign-in">
-          <li>Sign In</li>
-        </NavLink>
-        <NavLink activeClassName="selected" to="/sign-up">
-          <li>Sign Up</li>
-        </NavLink>
-      </ul>
-    );
-  };
 
   return (
     <div className="navigation">
       <div className="navigation__logo"></div>
-      {renderNavBar()}
+      <ul className="navigation__links">
+        {(logged ? loggedNav : unloggedNav).map(([text, link]) => (
+          <NavLink activeClassName="selected" to={link}>
+            <li>{text}</li>
+          </NavLink>
+        ))}
+      </ul>
     </div>
   );
 };
