4612a82be132133353807118abd4b5a57d97e126
dogrodnik <d.ogrodnik96@gmail.com>
2020-04-01 09:33:01 +0200

    Redux admin bug fix

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,13 +5,15 @@ import boardReducer from "./board/boardReducer";
 import bookingReducer from "./booking/bookingReducer";
 import userBookingReducer from "./userBookings/userBookingsReducer";
 import toastReducer from "./toast/toastReducer";
+import adminReducer from "./admin/adminReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   board: boardReducer,
   booking: bookingReducer,
   userBookings: userBookingReducer,
-  toast: toastReducer
+  toast: toastReducer,
+  admin: adminReducer
 });
 
 export default rootReducer;
