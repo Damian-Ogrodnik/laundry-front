ecd38767c69234e09e8721353334e620af4a40f2
dogrodnik <d.ogrodnik96@gmail.com>
2020-04-19 18:49:17 +0200

    Refactor Slots component

--- src/components/Slots/Slots.js
+++ src/components/Slots/Slots.js
@@ -6,18 +6,17 @@ import { Slot } from "../Slot";
 import { slots } from "../../utils/slots";
 
 export const Slots = () => {
-  const selectedSlot = useSelector(state => state.board.selectedSlot);
-  const takenSlots = useSelector(state => state.board.takenSlots);
-  const loggedUser = useSelector(state => state.user.id);
+  const { selectedSlot, takenSlots } = useSelector((state) => state.board);
+  const loggedUser = useSelector((state) => state.user.id);
 
   const renderSlots = () => {
     return slots.map(({ hours, number, lastBooking }) => {
       let taken, selected, user, id;
       if (takenSlots) {
-        taken = takenSlots.some(takenSlot => number === takenSlot.number);
+        taken = takenSlots.some((takenSlot) => number === takenSlot.number);
       }
       if (taken) {
-        takenSlots.forEach(takenSlot => {
+        takenSlots.forEach((takenSlot) => {
           if (takenSlot.number === number && takenSlot.user === loggedUser)
             user = true;
           id = takenSlot._id;
