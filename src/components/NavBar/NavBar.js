94607916042d519cd7a91333dcd5bf815208215c
dogrodnik <d.ogrodnik96@gmail.com>
2020-04-23 17:20:50 +0200

    Refactor navbar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,21 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
-const loggedNav = [
-  ["Account", "/user"],
-  ["Book", "/booking"],
-  ["My Bookings", "/user-bookings"],
-];
-const unloggedNav = [
-  ["Home", "/home"],
-  ["Sign In", "/sign-in"],
-  ["Sign Up", "/sign-up"],
-];
-
-const adminNav = [
-  ["Bookings", "/admin"],
-  ["Search", "/search"],
-];
+import * as data from "./utils";
 
 export const NavBar = () => {
   const { isAdmin, logged } = useSelector((state) => state.user);
@@ -25,13 +11,16 @@ export const NavBar = () => {
     <div className="navigation">
       <div className="navigation__logo"></div>
       <ul className="navigation__links">
-        {(!logged ? unloggedNav : isAdmin ? adminNav : loggedNav).map(
-          ([text, link]) => (
-            <NavLink key={text} activeClassName="selected" to={link}>
-              <li>{text}</li>
-            </NavLink>
-          )
-        )}
+        {(!logged
+          ? data.unloggedNav
+          : isAdmin
+          ? data.adminNav
+          : data.loggedNav
+        ).map(([text, link]) => (
+          <NavLink key={text} activeClassName="selected" to={link}>
+            <li>{text}</li>
+          </NavLink>
+        ))}
       </ul>
     </div>
   );
---
+++ src/components/NavBar/utils.js
@@ -0,0 +1,15 @@
+export const loggedNav = [
+  ["Account", "/user"],
+  ["Book", "/booking"],
+  ["My Bookings", "/user-bookings"],
+];
+export const unloggedNav = [
+  ["Home", "/home"],
+  ["Sign In", "/sign-in"],
+  ["Sign Up", "/sign-up"],
+];
+
+export const adminNav = [
+  ["Bookings", "/admin"],
+  ["Search", "/search"],
+];
