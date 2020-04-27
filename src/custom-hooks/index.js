d8d2d05e1e18a320705516e46eba031808629eca
dogrodnik <d.ogrodnik96@gmail.com>
2020-04-27 12:50:07 +0200

    index.html refactor

--- src/custom-hooks/index.js
+++ src/custom-hooks/index.js
@@ -6,7 +6,7 @@ import { checkAvailability } from "../services/Date";
 export const useDateCheck = (taken, lastBooking, selected = false, id) => {
   const [unavailable, setUnavailability] = useState(taken);
   const [style, setStyle] = useState("");
-  const date = useSelector(state => state.board.date);
+  const date = useSelector((state) => state.board.date);
 
   useEffect(() => {
     async function check() {
