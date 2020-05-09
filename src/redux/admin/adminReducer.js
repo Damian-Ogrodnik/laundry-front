634302432f4e47c06062ed8b3cf4ea32ad8960ac
dogrodnik <d.ogrodnik96@gmail.com>
2020-05-09 15:40:43 +0200

    Refactor admin reducer

--- src/redux/admin/adminReducer.js
+++ src/redux/admin/adminReducer.js
@@ -1,68 +1,42 @@
 import * as types from "./adminTypes";
 
 const initialState = {
-  loading: false,
-  error: null,
   choosedSlot: {},
   details: {},
+  error: null,
+  loading: false,
 };
 
 const adminReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
-    case types.CHOOSE_SLOT:
-      return { ...state, choosedSlot: { ...payload } };
-    case types.BOOK_SLOT_START:
+    case types.BOOK_SLOT_START ||
+      types.DELETE_SLOT_START ||
+      types.GET_DETAILS_START:
       return { ...state, loading: true, error: null };
-    case types.BOOK_SLOT_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        error: null,
-      };
-    case types.BOOK_SLOT_FAILURE:
-      return {
-        ...state,
-        loading: false,
-        error: payload.error,
-      };
-    case types.DELETE_SLOT_START:
-      return {
-        ...state,
-        error: null,
-        loading: true,
-      };
-    case types.DELETE_SLOT_SUCCESS:
-      return {
-        ...state,
-        error: null,
-        loading: false,
-      };
-    case types.DELETE_SLOT_FAILURE:
+    case types.BOOK_SLOT_FAILURE ||
+      types.DELETE_SLOT_FAILURE ||
+      types.GET_DETAILS_FAILURE:
       return {
         ...state,
         error: payload.error,
         loading: false,
       };
-    case types.GET_DETAILS_START:
+    case types.BOOK_SLOT_SUCCESS || types.DELETE_SLOT_SUCCESS:
       return {
         ...state,
         error: null,
-        loading: true,
+        loading: false,
       };
     case types.GET_DETAILS_SUCCESS:
       return {
         ...state,
+        details: { ...payload },
         error: null,
         loading: false,
-        details: { ...payload },
       };
-    case types.GET_DETAILS_FAILURE:
-      return {
-        ...state,
-        error: payload.error,
-        loading: false,
-      };
+    case types.CHOOSE_SLOT:
+      return { ...state, choosedSlot: { ...payload } };
     default:
       return state;
   }
