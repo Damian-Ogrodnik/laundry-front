09c1099bf927009a3d14471bb02268f50ac5e56e
dogrodnik <d.ogrodnik96@gmail.com>
2020-05-11 10:01:58 +0200

    Add UserList component

--- src/pages/AdminSearch/AdminSearch.js
+++ src/pages/AdminSearch/AdminSearch.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { Logout } from "../../components/Logout";
+import { UserList } from "../../components/adminComponents/UserList";
 import { UserSearch } from "../../components/adminComponents/UserSearch";
 
 export const AdminSearch = () => {
   return (
     <div className="admin-search">
       <UserSearch name={"admin-search__input"} />
+      <UserList />
       <Logout />
     </div>
   );
