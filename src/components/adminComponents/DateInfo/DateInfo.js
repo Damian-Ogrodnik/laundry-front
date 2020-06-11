5102cf9ac2863123b74b166a46e548e0bec40437
dogrodnik <d.ogrodnik96@gmail.com>
2020-06-11 11:11:56 +0200

    Refactor admin components

--- src/components/adminComponents/DateInfo/DateInfo.js
+++ src/components/adminComponents/DateInfo/DateInfo.js
@@ -1,21 +1,19 @@
 import React from "react";
 
-export const DateInfo = ({ date, text, time }) => {
-  return (
-    <>
-      <h2>{text}</h2>
-      <div className="modal__basics">
-        <div className="modal__date">
-          <p>
-            <span>Date:</span> {date}
-          </p>
-        </div>
-        <div className="modal__time">
-          <p>
-            <span>Time:</span> {time}
-          </p>
-        </div>
+export const DateInfo = ({ date, text, time }) => (
+  <>
+    <h2>{text}</h2>
+    <div className="modal__basics">
+      <div className="modal__date">
+        <p>
+          <span>Date:</span> {date}
+        </p>
       </div>
-    </>
-  );
-};
+      <div className="modal__time">
+        <p>
+          <span>Time:</span> {time}
+        </p>
+      </div>
+    </div>
+  </>
+);
--- src/components/adminComponents/UsersTable/Information/Information.js
+++ src/components/adminComponents/UsersTable/Information/Information.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-export const Information = () => {
-  return (
-    <tr>
-      <td className="empty" colSpan="2">
-        Nothing found. Try to search other term.
-      </td>
-    </tr>
-  );
-};
+export const Information = () => (
+  <tr>
+    <td className="empty" colSpan="2">
+      Nothing found. Try to search other term.
+    </td>
+  </tr>
+);
