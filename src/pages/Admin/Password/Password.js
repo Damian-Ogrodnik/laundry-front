d1c3270ee46fc2052323c535fe0cf262bd4a2697
dogrodnik <d.ogrodnik96@gmail.com>
2020-06-15 09:19:31 +0200

    Change password UI

--- src/pages/Admin/Password/Password.js
+++ src/pages/Admin/Password/Password.js
@@ -1,10 +1,55 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+
+import { adminChangePasswordSchema } from "services/validation";
+import { changePassword } from "redux/user/userUtils";
+
 import { BackArrow } from "components/BackArrow";
+import { FormWrapper } from "components/FormWrapper";
+import { Toast } from "components/Toast";
+
+const values = {
+  newPassword: "",
+  newPasswordConfirm: "",
+};
 
 export const Password = () => {
+  const dispatch = useDispatch();
   return (
     <div className="password">
       <BackArrow />
+      <Toast text={"Password Changed"} toastType={"CHANGE"} />
+      <Formik
+        initialValues={values}
+        validationSchema={adminChangePasswordSchema}
+        onSubmit={({ Password, newPassword }, { resetForm }) => {
+          resetForm({});
+          dispatch(changePassword(Password, newPassword));
+        }}
+      >
+        <Form className="password__form">
+          <FormWrapper
+            title={"Change Password"}
+            action={"CHANGE"}
+            render={() =>
+              Object.keys(values).map((value) => (
+                <div key={value} className="form__input">
+                  <label htmlFor={value}>
+                    {value === "newPassword"
+                      ? "New Password"
+                      : "Confirm New Password"}
+                  </label>
+                  <Field name={value} type="Password" />
+                  <ErrorMessage name={value}>
+                    {(msg) => <div className="form__error">{msg}</div>}
+                  </ErrorMessage>
+                </div>
+              ))
+            }
+          />
+        </Form>
+      </Formik>
     </div>
   );
 };
--- src/services/validation.js
+++ src/services/validation.js
@@ -4,28 +4,26 @@ export const registrationSchema = Yup.object().shape({
   Nickname: Yup.string()
     .max(15, "Must be 15 characters or less")
     .required("Required"),
-  Email: Yup.string()
-    .email("Invalid email address")
-    .required("Required"),
+  Email: Yup.string().email("Invalid email address").required("Required"),
   Password: Yup.string()
     .required("Required")
     .min(8, "Password must be at least 8 characters"),
   Repassword: Yup.string().when("Password", {
-    is: val => (val && val.length > 0 ? true : false),
+    is: (val) => (val && val.length > 0 ? true : false),
     then: Yup.string().oneOf(
       [Yup.ref("Password")],
       "Both password need to be the same"
-    )
-  })
+    ),
+  }),
 });
 
 export const loginSchema = Yup.object().shape({
   Nickname: Yup.string().required("Required"),
-  Password: Yup.string().required("Required")
+  Password: Yup.string().required("Required"),
 });
 
 export const nicknameSchema = Yup.object().shape({
-  Nickname: Yup.string().required("Required")
+  Nickname: Yup.string().required("Required"),
 });
 
 export const changePasswordSchema = Yup.object().shape({
@@ -34,10 +32,23 @@ export const changePasswordSchema = Yup.object().shape({
     .required("Required")
     .min(8, "Password must be at least 8 characters"),
   newPasswordConfirm: Yup.string().when("repassword", {
-    is: val => (val && val.length > 0 ? true : false),
+    is: (val) => (val && val.length > 0 ? true : false),
     then: Yup.string().oneOf(
       [Yup.ref("repassword")],
       "Both password need to be the same"
-    )
-  })
+    ),
+  }),
+});
+
+export const adminChangePasswordSchema = Yup.object().shape({
+  newPassword: Yup.string()
+    .required("Required")
+    .min(8, "Password must be at least 8 characters"),
+  newPasswordConfirm: Yup.string().when("repassword", {
+    is: (val) => (val && val.length > 0 ? true : false),
+    then: Yup.string().oneOf(
+      [Yup.ref("repassword")],
+      "Both password need to be the same"
+    ),
+  }),
 });
