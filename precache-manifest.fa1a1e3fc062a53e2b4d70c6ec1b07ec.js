36d111c923ea96eace7eabcd8ecf15289b271b8e
dogrodnik <d.ogrodnik96@gmail.com>
2020-06-17 09:59:20 +0200

    Updates

---
+++ precache-manifest.fa1a1e3fc062a53e2b4d70c6ec1b07ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d292dec300c69553436a42011cddb5",
+    "url": "/laundry/index.html"
+  },
+  {
+    "revision": "ee191e12274004466121",
+    "url": "/laundry/static/css/2.f477f4b5.chunk.css"
+  },
+  {
+    "revision": "22ce12a376db88652546",
+    "url": "/laundry/static/css/main.514f367f.chunk.css"
+  },
+  {
+    "revision": "ee191e12274004466121",
+    "url": "/laundry/static/js/2.623da6ad.chunk.js"
+  },
+  {
+    "revision": "bd85c3b1623efb25c2202260e8c02aee",
+    "url": "/laundry/static/js/2.623da6ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22ce12a376db88652546",
+    "url": "/laundry/static/js/main.e2c5b4a9.chunk.js"
+  },
+  {
+    "revision": "9e47a26a4562c1d01044",
+    "url": "/laundry/static/js/runtime-main.7dffa8ce.js"
+  },
+  {
+    "revision": "561d1603dca01c349d950a2f15b43db2",
+    "url": "/laundry/static/media/background-front.561d1603.jpg"
+  }
+]);
